bb77c10db43504218df7941bf2d080a87a64590a
conradkoh <conradkoh@gmail.com>
2025-03-01 12:42:29 +0800

    reliability: remove service worker

--- apps/webapp/src/app/layout.tsx
+++ apps/webapp/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ConvexClientProvider } from '@/components/atoms/contexts/ConvexClientProvider';
-import { ServiceWorkerRegistration } from '@/components/ServiceWorkerRegistration';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -55,10 +54,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ConvexClientProvider>
-          <ServiceWorkerRegistration />
-          {children}
-        </ConvexClientProvider>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
       </body>
     </html>
   );
